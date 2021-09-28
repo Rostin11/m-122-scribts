2b4ca4d12e8baed712bad438b10cd8326212f457
Rostin11 <robinstalder1@hotmail.com>
2021-09-28 16:05:49 +0200

    Update filediff.sh

--- filediff.sh
+++ filediff.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #
-# Name: entscheidung.sh - Überprüft, ob genug Argumente mitgegeben wurde.
+# Name: filediff.sh - Überprüft, ob genug Argumente mitgegeben wurde.
 # -----------------
-# SYNOPSIS: entscheidung.sh [OPTION]
+# SYNOPSIS: filediff.sh [OPTION]
 # Description: Überprüft, ob genug Argumente mitgegeben wurde.
-# entscheidung  -> Erste Datei
-# entscheidung -> Zweite Datei
+# filediff  -> Erste Datei
+# filediff -> Zweite Datei
 #
 # Autoren: R. Stalder
 # Version: 1.0.0
 # Datum: 14.9.2021
 #
-#
+# Aufrufen mit '. filediff.sh'
 
 if test $# -ne 2; then
-  echo "Sie müssen zwei Dateinamen als Argumente eingeben!"
-  echo "Usage: entscheidung.sh dateiname1 dateiname2"
+  echo "Sie mussen zwei Dateinamen als Argumente eingeben!"
+  echo "Usage: filediff.sh dateiname1 dateiname2"
 else
   if test -e $1; then
     echo "Die Datei existiert"
